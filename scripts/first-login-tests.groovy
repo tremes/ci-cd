ce3315c14f7b4b15a3a38d80701b4058ee7e4822
Tomáš Remeš <tremes@redhat.com>
2018-11-07 08:50:12 +0100

    Generate jobs in group & after first login tests.

---
+++ scripts/first-login-tests.groovy
@@ -0,0 +1,17 @@
+node('cirhos_rhel7'){
+        stage('Clone QE repository') {
+            dir('.') {
+                git branch: "${BRANCH}", url: "${REPOSITORY}"
+            }
+        }
+        stage('Run the smoke tests'){
+            dir('node-testsuite'){
+                sh '''
+                    oc login ${CLUSTER_URL} -u ${ADMIN_USERNAME} -p ${ADMIN_PASSWORD} --insecure-skip-tls-verify
+                    export USER_TOKEN=$(oc whoami -t)
+                    npm install
+                    gulp after-first-login
+                '''
+            }
+        }
+}
